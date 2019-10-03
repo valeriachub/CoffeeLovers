0e1971099d4f8c166baefab367de09d0156a3c48
Valeria Chub <valeria.chub@l1inc.com>
2019-10-03 16:35:45 +0300

    - Change version

--- CoffeeLovers/Helper/IngredientColor.swift
+++ CoffeeLovers/Helper/IngredientColor.swift
@@ -7,43 +7,42 @@
 //
 
 import UIKit
-import ChameleonFramework
 
 extension UIColor {
     
-    static func espressoColor() -> UIColor {
-        return UIColor(hexString: "#6F431B")
-    }
-    
-    static func waterIceColor() -> UIColor {
-        return UIColor(hexString: "#E2EBFD")
-    }
-    
-    static func milkColor() -> UIColor {
-        return UIColor(hexString: "#DCDFE3")
-    }
-    
-    static func milkfoamColor() -> UIColor {
-        return UIColor(hexString: "#DBCCB9")
-    }
-    
-    static func chocolateColor() -> UIColor {
-        return UIColor(hexString: "#361E0A")
-    }
-    
-    static func liquorColor() -> UIColor {
-        return UIColor(hexString: "#8C4B1A")
-    }
-    
-    static func icecreamColor() -> UIColor {
-        return UIColor(hexString: "#FEFFFF")
-    }
-    
-    static func whiskeyColor() -> UIColor {
-        return UIColor(hexString: "#BA6427")
-    }
-    
-    static func backgroudColor() -> UIColor {
-        return UIColor(hexString: "#2c281f")
-    }
+//    static func espressoColor() -> UIColor {
+//        return UIColor(hexString: "#6F431B")
+//    }
+//    
+//    static func waterIceColor() -> UIColor {
+//        return UIColor(hexString: "#E2EBFD")
+//    }
+//    
+//    static func milkColor() -> UIColor {
+//        return UIColor(hexString: "#DCDFE3")
+//    }
+//    
+//    static func milkfoamColor() -> UIColor {
+//        return UIColor(hexString: "#DBCCB9")
+//    }
+//    
+//    static func chocolateColor() -> UIColor {
+//        return UIColor(hexString: "#361E0A")
+//    }
+//    
+//    static func liquorColor() -> UIColor {
+//        return UIColor(hexString: "#8C4B1A")
+//    }
+//    
+//    static func icecreamColor() -> UIColor {
+//        return UIColor(hexString: "#FEFFFF")
+//    }
+//    
+//    static func whiskeyColor() -> UIColor {
+//        return UIColor(hexString: "#BA6427")
+//    }
+//    
+//    static func backgroudColor() -> UIColor {
+//        return UIColor(hexString: "#2c281f")
+//    }
 }
