1fe56fcee7dc4afe94466ad0caf2848c5bccedd1
Valeria Chub <valeriachub.dev@gmail.com>
2022-02-22 15:26:57 +0200

    Remove unused class

--- CoffeeLovers/Data Model/Coffee+CoreDataClass.swift
+++ CoffeeLovers/Data Model/Coffee+CoreDataClass.swift
@@ -9,7 +9,17 @@
 import Foundation
 import CoreData
 
-
+@objc(Coffee)
 public class Coffee: NSManagedObject {
-
+    @NSManaged public var calories_solo: Double
+    @NSManaged public var descriptions: String
+    @NSManaged public var image: String
+    @NSManaged public var is_favourite: Bool
+    @NSManaged public var title: String
+    @NSManaged public var calories_size: CaloriesSize?
+    
+    
+    var localCoffee: LocalCoffee {
+        return LocalCoffee(title: title, image: image, isFavourite: is_favourite, descriptions: descriptions)
+    }
 }
