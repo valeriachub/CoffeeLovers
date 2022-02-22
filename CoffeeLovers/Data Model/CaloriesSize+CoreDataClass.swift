a230ffe93b0be43a4211b3bab84a853f0b1150d3
Valeria Chub <valeriachub.dev@gmail.com>
2022-02-22 15:26:45 +0200

    Remove unused class

--- CoffeeLovers/Data Model/CaloriesSize+CoreDataClass.swift
+++ CoffeeLovers/Data Model/CaloriesSize+CoreDataClass.swift
@@ -11,5 +11,13 @@ import CoreData
 
 @objc(CaloriesSize)
 public class CaloriesSize: NSManagedObject {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CaloriesSize> {
+        return NSFetchRequest<CaloriesSize>(entityName: "CaloriesSize")
+    }
+
+    @NSManaged public var l: Double
+    @NSManaged public var m: Double
+    @NSManaged public var s: Double
+    @NSManaged public var coffee: Coffee?
 
 }
