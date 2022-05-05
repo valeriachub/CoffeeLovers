426948dc8b31fb7cb01d76a1f5829b7555044ebb
Valeria Chub <valeriachub.dev@gmail.com>
2022-05-05 21:47:11 +0200

    Init SUT does not receive any messages

--- CoffeeLoversTests/LoadCoffeeFromCacheUseCaseTests.swift
+++ CoffeeLoversTests/LoadCoffeeFromCacheUseCaseTests.swift
@@ -11,39 +11,47 @@ import CoffeeLovers
 
 class LoadCoffeeFromCacheUseCaseTests: XCTestCase {
 
-//    func test_init_doesNotReceiveAnyMessages() {
-//        let (_, store) = makeSUT()
-//
-//        XCTAssertEqual(store.messages, [])
-//    }
-//
-//    private func makeSUT() -> (sut: LocalDataLoader, store: CoffeeStoreSpy) {
-//        let store = CoffeeStoreSpy()
-//        let sut = LocalDataLoader(store: store)
-//        trackForMemoryLeaks(store)
-//        trackForMemoryLeaks(sut)
-//        return (sut, store)
-//    }
-//
-//    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-//
-//        addTeardownBlock { [weak instance] in
-//            XCTAssertNil(instance, "Instance should be deallocated. Potential memory leak", file: file, line: line)
-//        }
-//    }
-//
-//    private class CoffeeStoreSpy: CoffeeStore {
-//        enum Message {
-//            case retrieve
-//            case save
-//            case update
-//        }
-//
-//        var messages: [Message] = []
-//
-//        func retrieve(completion: @escaping (Result<[ManagedCoffee], Error>) -> Void) {
-//            messages.append(Message.retrieve)
-//        }
-//
-//    }
+    func test_init_doesNotReceiveAnyMessages() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.messages, [])
+    }
+
+    private func makeSUT() -> (sut: LocalDataLoader, store: CoffeeStoreSpy) {
+        let store = CoffeeStoreSpy()
+        let sut = LocalDataLoader(store: store)
+        trackForMemoryLeaks(store)
+        trackForMemoryLeaks(sut)
+        return (sut, store)
+    }
+
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should be deallocated. Potential memory leak", file: file, line: line)
+        }
+    }
+
+    private class CoffeeStoreSpy: CoffeeStore {
+        
+        enum Message {
+            case retrieve
+            case cache
+            case update
+        }
+
+        var messages: [Message] = []
+
+        func retrieve(completion: @escaping (Result<[ManagedCoffee], Error>) -> Void) {
+            messages.append(Message.retrieve)
+        }
+        
+        func cache(models: [CoffeeModel]) {
+            messages.append(Message.cache)
+        }
+        
+        func update(_ coffee: Coffee, completionSuccess: @escaping () -> Void) {
+            messages.append(Message.update)
+        }
+    }
 }
